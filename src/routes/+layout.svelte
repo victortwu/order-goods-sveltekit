e376731ef83cdb6de074563987c3a884b3dd9dfd
Victor Twu <victor607@gmail.com>
2023-08-18 17:17:56 -0700

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+    import '../app.postcss'
+</script>
+
+<main class='h-screen w-screen p-4 bg-slate-800 text-slate-200'>
+    <slot />
+</main>
---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,svelte,ts,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
