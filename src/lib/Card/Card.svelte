b39e0f6e658c9cd0aa194594cb4014ca0fd568a3
Victor Twu <victor607@gmail.com>
2023-08-25 17:34:24 -0700

    added some button logic to test

--- src/lib/Card/Card.svelte
+++ src/lib/Card/Card.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
     export let title: string = "This is the title";
-
+    export let description: string = "This is the description";
+    export let isAdd: boolean = true;
 </script>
 
 <div class="bg-violet-700 text-white max-w-xs overflow-hidden rounded-lg shadow-lg">
     <div class="px-6 py-4">
       <h4 class="mb-3 text-xl font-semibold tracking-tight">{title}</h4>
-      <p class="leading-normal">Lorem ipsum dolor, sit amet cons ectetur adipis icing elit. Praesen tium, quibusdam facere quo laborum maiores sequi nam tenetur laud.</p>
+      <p class="leading-normal">{description}</p>
+      <button class="float-right mb-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{isAdd ? "Add" : "Edit"}</button>
     </div>
   </div>
--- src/lib/Card/Card.test.ts
+++ src/lib/Card/Card.test.ts
@@ -3,6 +3,7 @@ import { describe, it, expect } from 'vitest';
 import Card from './Card.svelte';
 
 const mockTitleProp = 'This is the mock title!!';
+const mockDescriptionProp = 'This is the mock description!!';
 
 describe('Card', () => {
 	it('should render with title prop passed in', () => {
@@ -12,6 +13,26 @@ describe('Card', () => {
 		// select the title
 		const title = screen.getByText(mockTitleProp);
 		expect(title).toBeTruthy();
+		expect(container).toBeTruthy();
+	});
+
+	it('should render with description prop passed in', () => {
+		const { container } = render(Card, {
+			description: mockDescriptionProp
+		});
+		// select the description
+		const description = screen.getByText(mockDescriptionProp);
+		expect(description).toBeTruthy();
+		expect(container).toBeTruthy();
+	});
+
+	it('should render edit button when isAdd is false', () => {
+		const { container } = render(Card, {
+			isAdd: false
+		});
+		// select the edit button
+		const editButton = screen.getByText('Edit');
+		expect(editButton).toBeTruthy();
 		expect(container).toBeTruthy();
 	});
 });
