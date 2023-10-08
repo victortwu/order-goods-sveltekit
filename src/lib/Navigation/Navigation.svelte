d55470b7a7fb77378ffc809ed5428a0f92f9ddca
Victor Twu <victor607@gmail.com>
2023-10-07 22:17:05 -0700

    implemented initial navigation component

---
+++ src/lib/Navigation/Navigation.svelte
@@ -0,0 +1,6 @@
+<header class="fixed top-0 left-0 z-50 w-full shadow-lg">
+    <nav class="px-6 py-4 flex justify-between items-center text-slate-200 bg-slate-800">
+        <a href="/">Items Page</a>
+        <a href="/shoppingList">List Page</a>
+    </nav>
+</header>
