7aa9af543e8e3e905fa1096c1b52805fe5c7ca96
victor-twu <victor.twu@slalom.com>
2024-01-10 11:19:06 -0800

    updated mock data with upc field and uuids

--- src/lib/ProductList/ProductList.svelte
+++ src/lib/ProductList/ProductList.svelte
@@ -1,30 +1,37 @@
 <script lang="ts">
-    import Item from "$lib/Item/Item.svelte";
-    import { productList, shopppingList } from "./tempData";
+	import Item from '$lib/Item/Item.svelte';
+	import { productList, shopppingList } from './tempData';
 
-    let query = '';
+	let query = '';
 
-    const onInput = (e: Event) => {
-        query = (e.target as HTMLInputElement).value
-    }
+	const onInput = (e: Event) => {
+		query = (e.target as HTMLInputElement).value;
+	};
 
-    $: filterdList = productList.filter(item => {
-            if (query === '') {
-                return item
-            } else if (item.name.toLowerCase().includes(query.toLowerCase())) {
-                return item
-            }
-        })
-     
-    const isItInTheList =  (id: number) => {
-        return !!shopppingList.find(item => item.item.id === id)
-    }
+	$: filterdList = productList.filter((item) => {
+		if (query === '') {
+			return item;
+		} else if (item.name.toLowerCase().includes(query.toLowerCase())) {
+			return item;
+		}
+	});
+
+	const isItInTheList = (id: number | string) => {
+		return !!shopppingList.find((item) => item.item.id === id);
+	};
 </script>
 
-
-    <div class="flex flex-col w-full md:w-3/4 lg:w-1/2 gap-6">
-        <input on:input={(e) => onInput(e)} class="rounded p-2 bg-black text-slate-200 border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" id="search" type="text" maxlength="30" placeholder="Search" bind:value={query}/>
-        {#each filterdList as item}
-        <Item item={item} isEdit={isItInTheList(item.id)} />
-        {/each}
-    </div>
+<div class="flex flex-col w-full md:w-3/4 lg:w-1/2 gap-6">
+	<input
+		on:input={(e) => onInput(e)}
+		class="rounded p-2 bg-black text-slate-200 border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+		id="search"
+		type="text"
+		maxlength="30"
+		placeholder="Search"
+		bind:value={query}
+	/>
+	{#each filterdList as item}
+		<Item {item} isEdit={isItInTheList(item.id)} />
+	{/each}
+</div>
--- src/lib/ProductList/tempData.ts
+++ src/lib/ProductList/tempData.ts
@@ -2,140 +2,162 @@ import type { AddedItemType, ItemType } from '$lib/types';
 
 export const productList: ItemType[] = [
 	{
-		id: 20796680000,
+		id: '133f28af-727f-4885-8b5e-1d6ccf7754d0',
 		RDproductName: 'BF HALAL CHUCKROLL R/W',
 		name: 'Beef Chuck',
-		category: 'Food COGS'
+		category: 'Food COGS',
+		upc: 20796680000
 	},
 	{
-		id: 20713500000,
+		id: '4882be48-68e7-4089-bd03-d5aab34d1cd1',
 		RDproductName: 'LAMB GROUND',
 		name: 'Ground Lamb',
-		category: 'Food COGS'
+		category: 'Food COGS',
+		upc: 20713500000
 	},
 	{
-		id: 76069501695,
+		id: '3f3983fe-52d9-4b9e-a99c-d699a1062ac8',
 		RDproductName: 'YOG DAHI W/M JF 5#',
 		name: 'Yogurt',
-		category: 'Food COGS'
+		category: 'Food COGS',
+		upc: 76069501695
 	},
 	{
-		id: 20795020000,
+		id: '9d9561ba-d7d6-4397-a2d2-44062ffdf736',
 		RDproductName: 'CHX HAL THIGH CVP',
 		name: 'Chicken Thigh',
-		category: 'Food COGS'
+		category: 'Food COGS',
+		upc: 20795020000
 	},
 	{
-		id: 4900004422,
+		id: 'a65e66b0-0c54-4a20-b90f-709500b90432',
 		RDproductName: 'MEXICAN COKE 355ML',
 		name: 'Mexican Coke 12oz',
-		category: 'Food COGS'
+		category: 'Food COGS',
+		upc: 4900004422
 	},
 	{
-		id: 4900004897,
+		id: '50684c65-5f58-4f7a-9e0f-373285da68ad',
 		RDproductName: 'FANTA ORANGE 355ML',
 		name: 'Mexican Fanta 12oz',
-		category: 'Food COGS'
+		category: 'Food COGS',
+		upc: 4900004897
 	},
 	{
-		id: 61070809790,
+		id: '94e6d6a2-5e3e-4b8a-b01e-2f51d7205675',
 		RDproductName: 'PD HRB DILL 1LB',
 		name: 'Fresh Dill',
-		category: 'Food COGS'
+		category: 'Food COGS',
+		upc: 61070809790
 	},
 	{
-		id: 68044410345,
+		id: 'b73ffb66-ed35-43c1-8c37-307573a46e2b',
 		RDproductName: 'PD GARLIC PLD 5LB',
 		name: 'Peeled Garlic',
-		category: 'Food COGS'
+		category: 'Food COGS',
+		upc: 68044410345
 	},
 	{
-		id: 2446310124,
+		id: 'f204286d-0f12-4ca5-a6b0-92995a903cb8',
 		RDproductName: 'PASTE SAMBAL CHILI 136Z',
 		name: 'Sambal Oelek',
-		category: 'Food COGS'
+		category: 'Food COGS',
+		upc: 2446310124
 	},
 	{
-		id: 7934102213,
+		id: 'd5daae2d-37dc-4289-a585-2d0e5221b043',
 		RDproductName: 'TORT WHT GRDITA13"',
 		name: 'Wheat Tortilla',
-		category: 'Food COGS'
+		category: 'Food COGS',
+		upc: 7934102213
 	},
 	{
-		id: 78707400092,
+		id: '10becdae-083d-492c-860f-6f3ef4fe77bc',
 		RDproductName: 'PAPER WRAP 15X16',
 		name: 'Paper Sandwich Wrap',
-		category: 'Packaging COGS'
+		category: 'Packaging COGS',
+		upc: 78707400092
 	},
 	{
-		id: 72906299119,
+		id: '66fd33e0-e459-482e-bc75-ab5a632e3576',
 		RDproductName: 'PDU PARSLEY FLT 6C',
 		name: 'Flat Leaf Parsley',
-		category: 'Food COGS'
+		category: 'Food COGS',
+		upc: 72906299119
 	},
 	{
-		id: 3022301455,
+		id: 'a85ce649-bb61-4b7d-9d55-2ac7218c3783',
 		RDproductName: 'PD SPINACH CLEAN',
 		name: 'Spinach',
-		category: 'Food COGS'
+		category: 'Food COGS',
+		upc: 3022301455
 	},
 	{
-		id: 3338311902,
+		id: 'bf6c7fba-0298-4ce6-9672-f6c78c1f97c2',
 		RDproductName: 'PDU LEMON 5#',
 		name: 'Lemons',
-		category: 'Food COGS'
+		category: 'Food COGS',
+		upc: 3338311902
 	},
 	{
-		id: 76069557339,
+		id: 'f44ce04f-b9ec-4420-9808-4833e0d322b7',
 		RDproductName: 'SPC ONION PWDR',
 		name: 'Onion Powder',
-		category: 'Food COGS'
+		category: 'Food COGS',
+		upc: 76069557339
 	},
 	{
-		id: 76069557484,
+		id: '4541e5e5-97f8-4ce1-bdce-3015877b7b45',
 		RDproductName: 'SPC PAPRIKA 4.5LB',
 		name: 'Paprika',
-		category: 'Food COGS'
+		category: 'Food COGS',
+		upc: 76069557484
 	},
 	{
-		id: 60688610845,
+		id: 'e4fc8295-5551-4e43-8cfb-99b6f53cb4d9',
 		RDproductName: 'CHZ FETA CRUMB ITH 5#',
 		name: 'Feta Cheese',
-		category: 'Food COGS'
+		category: 'Food COGS',
+		upc: 60688610845
 	},
 	{
-		id: 72181004112,
+		id: 'f08007da-b821-45f9-8ce4-999b10ae5f25',
 		RDproductName: 'BAG SANDWICH-DX18',
 		name: 'Sandwich Bag #18',
-		category: 'Packaging COGS'
+		category: 'Packaging COGS',
+		upc: 72181004112
 	},
 	{
-		id: 76069502219,
+		id: '939be7e4-927b-49a4-a28e-36e6dda0b5bb',
 		RDproductName: 'CUP SOUF3.25ZSNSET',
 		name: 'To Go Sauce Containers 3.25 oz',
-		category: 'Packaging COGS'
+		category: 'Packaging COGS',
+		upc: 76069502219
 	},
 	{
-		id: 76069502959,
+		id: '7de22615-6bbb-4a4a-b068-d21872762acd',
 		RDproductName: 'CONT#3 TO GO KRAFT 64Z',
 		name: 'Platter/Salad To Go Containers #3',
-		category: 'Packaging COGS'
+		category: 'Packaging COGS',
+		upc: 76069502959
 	},
 	{
-		id: 76069502223,
+		id: '961a75b9-a0de-4a3f-9567-2031f08eafe5',
 		RDproductName: 'LID SFL 3.25-5Z',
 		name: 'To Go Sauce Container Lids',
-		category: 'Packaging COGS'
+		category: 'Packaging COGS',
+		upc: 76069502223
 	}
 ];
 
 export const shopppingList: AddedItemType[] = [
 	{
 		item: {
-			id: 76069501695,
+			id: '3f3983fe-52d9-4b9e-a99c-d699a1062ac8',
 			RDproductName: 'YOG DAHI W/M JF 5#',
 			name: 'Yogurt',
-			category: 'Food COGS'
+			category: 'Food COGS',
+			upc: 76069501695
 		},
 		quantity: {
 			number: 7,
@@ -144,10 +166,11 @@ export const shopppingList: AddedItemType[] = [
 	},
 	{
 		item: {
-			id: 68044410345,
+			id: 'b73ffb66-ed35-43c1-8c37-307573a46e2b',
 			RDproductName: 'PD GARLIC PLD 5LB',
 			name: 'Peeled Garlic',
-			category: 'Food COGS'
+			category: 'Food COGS',
+			upc: 68044410345
 		},
 		quantity: {
 			number: 3,
@@ -156,10 +179,11 @@ export const shopppingList: AddedItemType[] = [
 	},
 	{
 		item: {
-			id: 3338311902,
+			id: 'bf6c7fba-0298-4ce6-9672-f6c78c1f97c2',
 			RDproductName: 'PDU LEMON 5#',
 			name: 'Lemons',
-			category: 'Food COGS'
+			category: 'Food COGS',
+			upc: 3338311902
 		},
 		quantity: {
 			number: 1,
@@ -168,10 +192,11 @@ export const shopppingList: AddedItemType[] = [
 	},
 	{
 		item: {
-			id: 72181004112,
+			id: 'f08007da-b821-45f9-8ce4-999b10ae5f25',
 			RDproductName: 'BAG SANDWICH-DX18',
 			name: 'Sandwich Bag #18',
-			category: 'Packaging COGS'
+			category: 'Packaging COGS',
+			upc: 72181004112
 		},
 		quantity: {
 			number: 1,
@@ -180,10 +205,11 @@ export const shopppingList: AddedItemType[] = [
 	},
 	{
 		item: {
-			id: 78707400092,
+			id: '10becdae-083d-492c-860f-6f3ef4fe77bc',
 			RDproductName: 'PAPER WRAP 15X16',
 			name: 'Paper Sandwich Wrap',
-			category: 'Packaging COGS'
+			category: 'Packaging COGS',
+			upc: 78707400092
 		},
 		quantity: {
 			number: 2,
@@ -192,10 +218,11 @@ export const shopppingList: AddedItemType[] = [
 	},
 	{
 		item: {
-			id: 76069502219,
+			id: '939be7e4-927b-49a4-a28e-36e6dda0b5bb',
 			RDproductName: 'CUP SOUF3.25ZSNSET',
 			name: 'To Go Sauce Containers 3.25 oz',
-			category: 'Packaging COGS'
+			category: 'Packaging COGS',
+			upc: 76069502219
 		},
 		quantity: {
 			number: 1,
